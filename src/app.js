0d7fd9f7f178f808316625836d909f279cfb9379
GhostGirl03 <152809822+GhostGirl03@users.noreply.github.com>
2024-03-05 00:31:58 +0000

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const pronoun = ["me", "you"];
+const adj = ["pretty", "brilliant"];
+const noun = ["people", "computer"];
+
+const domains = [];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  for (const i of pronoun) {
+    for (const j of adj) {
+      for (const k of noun) {
+        domains.push(i + j + k);
+      }
+    }
+  }
+  document.querySelector("div").innerHTML = domains.join("<br/>");
 };
